da820dbe3dd69bf34309bed23354f15d813915b1
eladahdut <eladahdut@gmail.com>
2022-01-14 15:01:01 +0200

    done

--- abra-weather-app/src/API/getApi.js
+++ abra-weather-app/src/API/getApi.js
@@ -8,17 +8,17 @@ const apiKey = "xC8As2or5WtHRfh9zHDFAkkmdm3QqH7m"
 
 
 export async function getAutoComplete(q) {
-    const response = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${q}`);
+    const response = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${q}`);
     return Array.isArray(response.data) ? response.data : [];
 }
 
 export async function getCurrentCondition(locationKey = "215854") {
-    const response = await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`);
+    const response = await axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`);
     return response.data;
 }
 
 export async function getFiveDays(locationKey = "215854") {
-    const response = await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}`);
+    const response = await axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}`);
     return response.data;
 }
 
